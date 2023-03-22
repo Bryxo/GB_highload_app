8b01b6218732864c773210e1b790aecbba903128
Bryxo <bryxo.job@yandex.ru>
2023-03-22 20:21:57 +0300

    homework3

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+    return { message: 'Здесь могла быть ваша реклама!! Но преподам geekbrains вообще похер=)' };
   }
 }
